04a3a09892ac4dd901eeba60ac55661b51a266af
Menard Manlapaz <menardmanlapaz@skydev.solutions>
2024-05-30 14:48:08 +0800

    :sparkles: add string

---
+++ string.php
@@ -0,0 +1,12 @@
+<?php
+//  How to concat string ? write example code
+$string1 = "hello";
+$string2 = "world";
+$con = $string1 . " " . $string2;
+echo $con;
+
+ //  How to replace content of string ? write example code
+ $new_string = str_replace("world", "php", $con);
+ echo $new_string;
+
+?>
